0a5788cefc7242b5d19d7b8016d593b802ae660e
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2024-04-24 01:44:44 +0530

    Bubble_Sort.cpp

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter count : ";     // element count in array
+  cin>>n;
+
+  int a[n] = { };         
+
+  
+  for ( int i=0; i<n; i++ ){
+    cout<<"enter element : ";
+    cin>>a[i];                     // inserting elements in array
+  }
+
+  cout<<"\narray : { ";
+  for ( int i=0; i<n; i++ ){
+    cout<<"\t"<<a[i];              // displaying  elements from array
+  }
+  cout<<" }";
+
+                              // Bubble sorting
+  int temp = 0;
+  for ( int i=0; i<n; i++ ){
+    for ( int j=0; j<n; j++ ){
+      if( a[i]>a[j]){
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+
+
+  cout<<"\n\n sorted array : { ";
+  for ( int i=0; i<n; i++ ){
+    cout<<"\t"<<a[i];              // displaying  sorted array
+  }
+  cout<<" }";
+  
+}
