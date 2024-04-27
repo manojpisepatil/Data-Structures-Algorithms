b677a8dc934eb648154feaaace5b2b99d46e8439
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2024-04-27 23:31:10 +0530

    simple_class.cpp

---
+++ simple_class.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// create a class
+class Room {
+
+   public:
+    double length;
+    double breadth;
+    double height;
+
+    double calculate_area() {
+        return length * breadth;
+    }
+
+    double calculate_volume() {
+        return length * breadth * height;
+    }
+};
+
+int main() {
+
+    // create object of Room class
+    Room room1;
+
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculate_area() << endl;
+    cout << "Volume of Room =  " << room1.calculate_volume() << endl;
+
+    return 0;
+}
