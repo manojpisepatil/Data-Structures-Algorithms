ecba4932505f51ee8690709e565650b26004f8d8
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2024-05-02 23:53:45 +0530

    HierarchicalInheritance.cpp

---
+++ HierarchicalInheritance.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// base class
+class Animal {
+public:
+    void info() {
+        cout << "I am an animal." << endl;
+    }
+};
+
+// derived class 1
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "I am a Dog. Woof woof." << endl;
+    }
+};
+
+// derived class 2
+class Cat : public Animal {
+public:
+    void meow() {
+        cout << "I am a Cat. Meow." << endl;
+    }
+};
+
+int main() {
+    // create object of Dog class
+    Dog dog1;
+    cout << "Dog Class:" << endl;
+    dog1.info();  // parent Class function
+    dog1.bark();
+
+    // create object of Cat class
+    Cat cat1;
+    cout << "\nCat Class:" << endl;
+    cat1.info();  // parent Class function
+    cat1.meow();
+
+    return 0;
+}
