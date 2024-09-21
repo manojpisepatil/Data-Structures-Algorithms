62674f6d63659440aedeb534b20d388ecc0559a0
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2024-09-21 23:48:50 +0530

    FLOYDsTrianglePattern.java
    FLOYD'S Triangle Pattern
    1
    2  3
    4  5  6
    7  8  9  10
    11 12 13 14 15

---
+++ Pattern's/FLOYDsTrianglePattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FLOYDsTrianglePattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+      
+// FLOYD'S Triangle Pattern
+// 1
+// 2  3
+// 4  5  6
+// 7  8  9  10
+// 11 12 13 14 15
+
+
+        int n = 5;
+        int k = 1;
+        for(int i=1; i<=n; i++){
+                for(int j=1; j<=i; j++){
+                        System.out.print(" "+k);
+                        k++;
+                }
+                System.out.println();
+        }
+    }
+}
